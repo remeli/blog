9bae931199b5ba0a16584ed27f384d1687a1647f
bek <obozrevatel90@gmail.com>
2011-12-13 19:43:34 +0600

    rake migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 
 ActiveRecord::Schema.define(:version => 20111213112121) do
 
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "description"
