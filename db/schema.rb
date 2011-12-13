1f3dd811e3ff20ddab0b7f37cace0e3a49ecf868
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-13 17:28:04 +0600

    Add user model, edit user migrate file

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111213085112) do
+ActiveRecord::Schema.define(:version => 20111213112121) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
