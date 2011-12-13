b3d402cb5adb738080e5bf5797bd9adb54b992af
Mons Writer <monswriter@gmail.com>
2011-12-13 15:48:46 +0600

    migration: added category_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111213085112) do
+ActiveRecord::Schema.define(:version => 20111213093357) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end
