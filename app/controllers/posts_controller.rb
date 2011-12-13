2c53b1013ce1659602290bf0ba18af53491f17a3
bek <obozrevatel90@gmail.com>
2011-12-13 16:42:44 +0600

    validates && post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,62 @@
 class PostsController < ApplicationController
+  
+  def index
+    @posts=Post.all
+    respond_to do |f|
+    f.html
+    f.json {render :json=>@posts}
+    end
+    
+  end
+  
+  def show
+  
+    @post.Post.find(paramd[:id])
+    respond_to do |f|
+    f.html
+    f.json {render :json=>@post}
+    end
+    
+  end
+  
+  def new
+    @post=Post.new
+    respond_to do |f|
+    f.html
+    f.json {render :json=>@post}
+    end
+  end
+  
+  def edit
+  @post=Post.find(params[:id])
+    
+  end
+  
+  def create
+    @post=Post.new(params[:post])
+    respond_to do |f|
+      f.html { redirect_to @post, :notice=>'Post created!'}
+      f.json {render :json=>@post}
+    end    
+  end
+  
+  def update
+    @post=Post.find(params[:id])
+    
+    respond_to do |f|
+      format.html { redirect_to @post, :notice => 'Post updated!' }
+      format.json { head :ok }
+    end    
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    respond_to do |format|
+    format.html { redirect_to post_url }
+    format.json { head :ok }
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+validates :title, :description, :presence=>true
+validates :title, :uniqueness=>true, :length =>{ :within => 2..25 } 
 end
