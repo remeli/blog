11360df2e5057e741633d81a1a0ac39c6b16a8e0
bek <obozrevatel90@gmail.com>
2011-12-14 11:18:00 +0600

    belongs_to :user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 #encoding:UTF-8
 class Post < ActiveRecord::Base
 belongs_to :category
+belongs_to :user
 validates :title, :description, :presence=>true
 validates :title, :uniqueness=>true, :length =>{ :within => 2..25 } 
 end
