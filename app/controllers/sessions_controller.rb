9a7f0b45bafb351b4ac33cd0bc289fb1b6dacb1a
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-14 17:21:30 +0600

    Log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,5 +17,10 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Вы успешно вышли"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Blog::Application.routes.draw do
   
   match "sign_up" => "users#new", :as => "sign_up"
   match "log_in" => "sessions#new", :as => "log_in"
-  
+  match "log_out" => "sessions#destroy", :as => "log_out"
   resources :categories
   resources :sessions
   resources :users
