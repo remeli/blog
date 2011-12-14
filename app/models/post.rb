01fb3bdebbea35a9a56f6495fbec260a278c9255
Mons Writer <monswriter@gmail.com>
2011-12-14 15:46:34 +0600

    updated post.rb model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 #encoding:UTF-8
+
 class Post < ActiveRecord::Base
-belongs_to :category
-belongs_to :user
-validates :title, :description, :presence=>true
-validates :title, :uniqueness=>true, :length =>{ :within => 2..25 } 
+  belongs_to :category
+  belongs_to :user
+  has_many :comments, :dependent => :destroy
+  validates :title, :description, :presence => true
+  validates :title, :uniqueness => true, :length =>{ :within => 2..25 } 
 end
