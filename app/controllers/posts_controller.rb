42ed9db5b419dd0aed42eb52f9efc42dd28d2fb8
Mons Writer <monswriter@gmail.com>
2011-12-14 17:13:05 +0600

    post views(without comments for the present) and updated posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,25 @@
 #encoding:UTF-8
+
 class PostsController < ApplicationController
   
   def index
-    @posts=Post.all
-    #redirect_to posts_path(@posts) 
+    @posts = Post.all
   end
   
   def show
-    @post.Post.find(paramd[:id])
-    redirect_to post_path(@post)    
+    @post = Post.find(params[:id])
   end
   
   def new
-    @post=Post.new
+    @post = Post.new
   end
   
   def edit
-  @post=Post.find(params[:id])     
+    @post = Post.find(params[:id])     
   end
   
   def create
-    @post=Post.new(params[:post])
+    @post = Post.new(params[:post])
     if @post.save
       redirect_to post_path(@post)
     else
@@ -29,7 +28,7 @@ class PostsController < ApplicationController
   end
   
   def update
-    @post=Post.find(params[:id])
+    @post = Post.find(params[:id])
     if @post.update_attributes(params[:post])
       redirect_to post_path(@post)
     else
