ed6c21557869b29653cc8b51a86931f13e570615
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-14 16:22:26 +0600

    create method in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,17 @@ class SessionsController < ApplicationController
   
   def new
   end
+  
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      flash[:notice] = 'Вы успешно вошли'
+      respond_with(:location => root_url)
+    else
+      flash.now.alert = 'Не верный пароль или email'
+      render 'new'
+    end
+  end
 
 end
