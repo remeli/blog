96b808b704f7b23cdfd46ed97d31b36fb66f98f1
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-14 17:11:28 +0600

    edit flash messages, stylized

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+# encoding:UTF-8
 class SessionsController < ApplicationController
   
   respond_to :html
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,13 @@ class User < ActiveRecord::Base
     end
   end
   
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+  
 end
