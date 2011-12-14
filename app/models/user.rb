0e9a0484977f4ed9bafebc776bdc91e8ce628211
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-14 13:35:13 +0600

    Validates, attr_accessor password -> User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 #encoding:UTF-8
 class User < ActiveRecord::Base
+  attr_accessor :password
+  validates :password, :confirmation => true
+  validates :password, :presence => true, :on => :create
+  validates :email, :presence => true
+  valudates :email, :uniqueness => true
 end
