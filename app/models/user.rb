c0d068a7d1c4d0df0345833706d73dd893dc192d
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-14 14:00:12 +0600

    Encrypted password

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,18 @@ class User < ActiveRecord::Base
   #
   
   attr_accessor :password
+  before_save :encrypt_password
+  
   validates :password, :confirmation => true
   validates :password, :presence => true, :on => :create
   validates :email, :presence => true
   valudates :email, :uniqueness => true
+  
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+  
 end
