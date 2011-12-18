329c43e741923886cabaeca03c9556bb24c806da
Mons Writer <monswriter@gmail.com>
2011-12-18 13:51:57 +0600

    updated comments_controller and comment partials(stable and working)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,12 @@
 
 class CommentsController < ApplicationController
   
-  before_filter :find_post
-  
   def new
     @comment = Comment.new
   end
   
   def create
+    
     @comment = Comment.new(params[:comment])
     if @comment.save
       redirect_to :back
@@ -24,12 +23,6 @@ class CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-  end
-  
-  private
-  
-  def find_post
-    @post = Post.find(params[:post_id])
   end
   
 end
