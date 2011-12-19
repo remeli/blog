f4c8ed404601ea99eca083666d32a7c0de9afe8e
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-19 18:36:35 +0600

    helper method, admin?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 # encoding:UTF-8
 module ApplicationHelper
+  
   def errors_for_object(object)
     render(:partial => "shared/errors", :locals => { :object => object}) if object.errors.any?
   end
+  
+  def admin?(user)
+    true if user.verify_access
+  end
+  
 end
