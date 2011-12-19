a3dcf26994e3c2b21f136a3465b1fa74ab731629
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-19 16:46:08 +0600

    partial for errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # encoding:UTF-8
 module ApplicationHelper
+  def errors_for_object(object)
+    render(:partial => "shared/errors", :locals => { :object => object}) if object.errors.any?
+  end
 end
