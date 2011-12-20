c2f51da3de141ff42c526fce2e268fbb0a92b64a
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-20 19:29:45 +0600

    fix posts controller/model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   respond_to :html
   
   def index
-    @posts = Post.order('created_at DESC').page(params[:page]).per(2)
+    @posts = Post.page(params[:page]).per(5)
     respond_with @posts
   end
     
