c2a6932c8f9e2eb174121257516d037ff682534e
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-20 16:24:56 +0600

    edit admin/userscontroller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,9 +4,11 @@ class Admin::UsersController < ApplicationController
   
   layout 'admin'
   before_filter :access
+  respond_to :html
   
   def index
-
+    @users = User.all
+    respond_with(@users)
   end
   
 end
