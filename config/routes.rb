7ff7b566e523495a0db0c3d147642e99acaf523b
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-20 16:34:56 +0600

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Blog::Application.routes.draw do
-
-  resources :comments
-  resources :pages
-  resources :categories, :only => [:index, :show]
-
+  
   match "sign_up" => "users#new", :as => "sign_up"
   match "log_in" => "sessions#new", :as => "log_in"
   match "log_out" => "sessions#destroy", :as => "log_out"
   resources :sessions
-  resources :users
+  
+  resources :users, :only => [:new, :create]
   resources :posts, :only => [:index, :show, :new, :create]
+  resources :categories, :only => [:index, :show]
+  resources :comments
+  resources :pages
+  
   
   # match '/:permalink', :to => "pages#show"
   match 'static/:permalink', :to => "pages#show"
@@ -17,9 +18,9 @@ Blog::Application.routes.draw do
   # admin:
   match "admin" => "admin#index", :as => "admin"
   namespace :admin do
-    resources :categories
     resources :posts, :except => [:new, :create]
-    resources :users
+    resources :users, :except => [:new, :create]
+    resources :categories
     resources :pages
   end
 
