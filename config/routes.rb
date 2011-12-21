055be1751c04c2598e478874d1fec8f25bf2fabf
Yerlan Kussainov <lagox.mail@gmail.com>
2011-12-21 18:33:24 +0600

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Blog::Application.routes.draw do
   
   # resources:
   resources :sessions
-  resources :users, :only => [:new, :create]
+  resources :users, :only => [:index, :show, :new, :create]
   resources :posts, :only => [:index, :show, :new, :create] do
     resources :comments
     get "page/:page.:format", :action => :index, :on => :collection, :defaults => { :format => "html"}
